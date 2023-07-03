1a4749440436999298b50f1536607b0874abf9a6
ian-t-adams <ianadams@microsoft.com>
2023-07-03 16:01:07 -0500

    added gpt-35-turbo-16k and fixing configs

--- src/aoai_streamlit_app.py
+++ src/aoai_streamlit_app.py
@@ -1,130 +1,121 @@
-
-import os
-import openai
-import streamlit as st
-#do this to load the env variables
-from dotenv import load_dotenv
-
-load_dotenv()
-
-if __name__ == "__main__":
-    # Set up OpenAI
-    openai.api_type="azure"
-    openai.api_key=os.environ['APIM_KEY']
-    openai.api_base=os.environ['APIM_ENDPOINT']
-    openai.api_version =os.environ['AOAI_API_VERSION']
-
-    # The code gets the response from OpenAI, formats it, and writes it to the result box
-    def get_openai_response(prompt, max_tokens, temperature, model, streaming):
-        try:
-            # If streaming, loop through the response and append to a list
-            if streaming == "Streaming":
-                report = []
-                # Looping over the response
-                for resp in openai.Completion.create(engine=model,
-                                                    prompt=prompt,
-                                                    max_tokens=max_tokens, 
-                                                    temperature=temperature,
-                                                    n=1,
-                                                    stream=True):
-                    report.append(resp['choices'][0]['text'])
-                    result = "".join(report).strip()
-                    result = result.replace("\n", "")       
-                    res_box.markdown(f'*{result}*') 
-
-            # If not streaming, get the response and write to the result box
-            else:
-                completions = openai.Completion.create(engine=model,
-                                                    prompt=prompt,
-                                                    max_tokens=max_tokens, 
-                                                    temperature=temperature,
-                                                    stream=False)
-                result = completions.choices[0].text
-
-                res_box.write(result)
-        # Catch OpenAI rate limit errors
-        except openai.error.RateLimitError as e:
+import os  
+import openai  
+import streamlit as st  
+from dotenv import load_dotenv  
+  
+load_dotenv()  
+  
+if __name__ == "__main__":  
+    openai.api_type = "azure"  
+    openai.api_key = os.environ['APIM_KEY']  
+    openai.api_base = os.environ['APIM_ENDPOINT']  
+    openai.api_version = os.environ['AOAI_API_VERSION']  
+  
+    def get_openai_response(prompt, max_tokens, temperature, model, streaming):  
+        try:  
+            if streaming == "Streaming":  
+                report = []  
+                for resp in openai.Completion.create(engine=model,  
+                                                     prompt=prompt,  
+                                                     max_tokens=max_tokens,  
+                                                     temperature=temperature,  
+                                                     n=1,  
+                                                     stream=True):  
+                    report.append(resp['choices'][0]['text'])  
+                    result = "".join(report).strip()  
+                    result = result.replace("\n", "")  
+                    res_box.markdown(f'*{result}*')  
+  
+            else:  
+                completions = openai.Completion.create(engine=model,  
+                                                       prompt=prompt,  
+                                                       max_tokens=max_tokens,  
+                                                       temperature=temperature,  
+                                                       stream=False)  
+                result = completions.choices[0].text  
+  
+                res_box.write(result)  
+        except openai.error.RateLimitError as e:  
+            raise e  
+  
+    def get_openai_Chat(messages, max_tokens, temperature, model, streaming):  
+        try:  
+            if streaming == "Streaming":  
+                report = []  
+                for resp in openai.ChatCompletion.create(engine=model,  
+                                                         messages=messages,  
+                                                         max_tokens=max_tokens,  
+                                                         temperature=temperature,  
+                                                         stream=True):  
+                    if 'content' in resp['choices'][0]['delta']:  
+                        report.append(resp['choices'][0]['delta']['content'])  
+                        result = "".join(report).strip()  
+                        result = result.replace("\n", "")  
+                        res_box.markdown(f'*{result}*')  
+  
+            else:  
+                completions = openai.ChatCompletion.create(engine=model,  
+                                                           messages=messages,  
+                                                           max_tokens=max_tokens,  
+                                                           temperature=temperature,  
+                                                           stream=False)  
+                result = completions.choices[0].message.content  
+  
+                res_box.write(result)  
+        except openai.error.RateLimitError as e:  
             raise e
 
-    def get_openai_Chat(messages, max_tokens, temperature, model, streaming):
-        try:
-            # If streaming, loop through the response and append to a list
-            if streaming == "Streaming":
-                report = []
-                # Looping over the response
-                for resp in openai.ChatCompletion.create(engine=model,
-                                                        messages=messages,
-                                                        max_tokens=max_tokens, 
-                                                        temperature=temperature,
-                                                        stream=True):
-                    if 'content' in resp['choices'][0]['delta']:
-                        report.append(resp['choices'][0]['delta']['content'])
-                        result = "".join(report).strip()
-                        result = result.replace("\n", "")       
-                        res_box.markdown(f'*{result}*') 
+    st.sidebar.title("Configuration")  
 
-            # If not streaming, get the response and write to the result box
-            else:
-                completions = openai.ChatCompletion.create(engine=model,
-                                                            messages=messages,
-                                                            max_tokens=max_tokens, 
-                                                            temperature=temperature,
-                                                            stream=False)
-                result = completions.choices[0].message.content
+    # Create a dictionary containing the available models for each completion type  
+    available_models = {  
+        "Chat": ["gpt-4", "gpt-4-32k", "gpt-35-turbo", "gpt-35-turbo-16k"],  
+        "Completion": ["text-davinci-003"],  
+        "Embedding": ["text-embedding-ada-002"]  
+    }
 
-                res_box.write(result)
-        except openai.error.RateLimitError as e:
-            raise e       
+    callEndpoints = ["Chat", "Completion", "Embedding"]  
+    completion_type = st.sidebar.radio(  
+        "Choose whether you wish to engage in a Chat (ChatCompletions) or Completions interaction for text or Embeddings for a demonstration of embeddings outputs:",  
+        callEndpoints)
 
-    st.subheader("Chat, Stream, Retry")  
-    
-    options=["Streaming", "No Streaming"]  
-    selected = st.radio("Choose whether to stream responses or not:", options)  
-    
-    callEndpoints=["Chat", "Completion", "Embedding"]  
-    endpointOpt = st.radio("Choose whether you wish to engage in a Chat (ChatCompletions) or Completions interaction for text or Embeddings for a demonstration of embeddings outputs:",callEndpoints)  
-    
-    if endpointOpt == "Chat":  
-        model_options = ["gpt-35-turbo", "gpt-4", "gpt-4-32k"]  
-    elif endpointOpt == "Completion":  
-        model_options = ["text-davinci-003"]  
-    else:  
-        model_options = ["text-embedding-ada-002"]  
-    model = st.selectbox("Choose a model:", model_options)  
-    
-    # Max tokens selection with Streamlit slider  
-    if model == "gpt-35-turbo":  
-        max_tokens = st.slider("Choose max tokens:", min_value=10, max_value=4000, step=10)
-        temp_options = st.slider("Choose a temperature:", min_value=0.00, max_value=2.00, step=0.01)
-        top_p_options = st.slider("Choose a top p:", min_value=0.00, max_value=1.00, step=0.01)
-    elif model == "gpt-4":
-        max_tokens = st.slider("Choose max tokens:", min_value=10, max_value=8190, step=10)
-        temp_options = st.slider("Choose a temperature:", min_value=0.00, max_value=2.00, step=0.01)
-    elif model == "gpt-4-32k":
-        max_tokens = st.slider("Choose max tokens:", min_value=10, max_value=32760, step=10)
-        temp_options = st.slider("Choose a temperature:", min_value=0.00, max_value=2.00, step=0.01)
-    elif model == "text-davinci-003":
-        max_tokens = st.slider("Choose max tokens:", min_value=10, max_value=4000, step=10)
-        temp_options = st.slider("Choose a temperature:", min_value=0.00, max_value=1.00, step=0.01)
-    else:
-        max_tokens = st.slider("Choose max tokens:", min_value=10, max_value=4000, step=10)
-        temp_options = st.slider("Choose a temperature:", min_value=0, max_value=0, step=0)  
-    
-    user_input = st.text_input("You: ", placeholder = "Ask me anything ...", value="Tell me a short joke",key="input")  
-    
+    # Update the model_options depending on the selected completion type  
+    model_options = available_models[completion_type]    
+  
+    models_explanation = st.sidebar.expander("Models Explanation")  
+    models_explanation.write("Different AI models have different capabilities and limitations. Some models may be better suited for specific tasks, while others may offer faster response times or more creative responses. Choose a model that best fits your needs.")  
+      
+    model = st.sidebar.selectbox("Choose a model:", model_options)  
+  
+    max_tokens_explanation = st.sidebar.expander("Max Tokens Explanation")  
+    max_tokens_explanation.write("Max tokens determine the maximum length of the AI response. A larger value allows for longer, more detailed responses, while a smaller value will limit the response to a shorter length.")  
+  
+    max_tokens = st.sidebar.slider("Choose max tokens:", min_value=10, max_value=4000, step=10)  
+  
+    temperature_explanation = st.sidebar.expander("Temperature Explanation")  
+    temperature_explanation.write("Temperature affects the randomness of the AI response. A higher temperature will result in more random and creative responses, while a lower temperature will produce more focused and deterministic responses.")  
+  
+    temp_options = st.sidebar.slider("Choose a temperature:", min_value=0.00, max_value=2.00, step=0.01)  
+  
+    options = ["Streaming", "No Streaming"]  
+    selected = st.sidebar.radio("Choose whether to stream responses or not:", options)  
+  
+    st.subheader("Chat with AOAI!")  
+    user_input = st.text_input("You: ", placeholder="Ask me anything ...", value="Tell me a short joke", key="input")  
+  
     if st.button("Submit", type="primary"):  
-        st.markdown("----")
-
-        res_box = st.empty()
-        exp = st.expander("See more info")
-
-        messages=[]
-        messages.append({"role":"user","content":user_input})
-
-        streaming=True if selected == "Streaming" else False
-        if endpointOpt == "Completion":
-            get_openai_response(user_input, max_tokens, temp_options, model, streaming)
-        else:
-            get_openai_Chat(messages, max_tokens, temp_options, model, streaming)
-
-    st.markdown("----")
+        st.markdown("----")  
+  
+        res_box = st.empty()  
+        exp = st.expander("See more info")  
+  
+        messages = []  
+        messages.append({"role": "user", "content": user_input})  
+  
+        streaming = True if selected == "Streaming" else False  
+        if completion_type == "Completion":  
+            get_openai_response(user_input, max_tokens, temp_options, model, streaming)  
+        else:  
+            get_openai_Chat(messages, max_tokens, temp_options, model, streaming)  
+  
+    st.markdown("----")  
